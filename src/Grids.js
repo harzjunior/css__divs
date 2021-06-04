26f1259433104507d888decedc0ffae139720d40
harzjunior <harunjibs@gmail.com>
2021-06-04 05:22:41 +0300

    init

---
+++ src/Grids.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+function Grids() {
+  return (
+    <div>
+      <div className="grid__container">
+        <div className="grid__item1">
+          <h1>Grid__item__1</h1>
+          <p className='p__item1'>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </p>
+        </div>
+          <div className="grid__item2">
+            <h1>Grid__item__2</h1>
+          </div>
+          <div className="grid__item3">
+            <h1>Grid__item__3</h1>
+          </div>
+          <div className="grid__item4">
+            <h1>Grid__item__4</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div className="grid__item5">
+            <h1>Grid__item__5</h1>
+          </div>
+          <div className="grid__item6">
+            <h1>Grid__item__6</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div className="grid__item7">
+            <h1>Grid__item__7</h1>
+          </div>
+          <div className="grid__item8">
+            <h1>Grid__item__8</h1>
+          </div>
+          <div className="grid__item9">
+            <h1>Grid__item__9</h1>
+          </div>
+          <div className="grid__item10">
+            <h1>Grid__item__10</h1>
+          </div>
+          <div className="grid__item11">
+            <h1>Grid__item__11</h1>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default Grids;
